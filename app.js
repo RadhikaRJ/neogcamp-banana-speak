10540f4e0835852283cae2709b9a2e498bece4d2
RadhikaRJ <radhika7002@gmail.com>
2020-12-04 22:56:03 +0530

    Input-Process-Output Textarea-clickEventHandler-OutputInDiv

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var btntranslate = document.querySelector("#btn-translate");
-btntranslate.addEventListener("click",clickEventHandler());
+var textInput = document.querySelector("#txt-input");
+var textOutputDiv =document.querySelector("#output-txt");
+// console.log(textInput);
+btntranslate.addEventListener("click",clickEventHandler);
 function clickEventHandler(){
     console.log("Button is clicked!");
-}
+    console.log("text input: ",textInput.value);
+    textOutputDiv.innerText ="Hello "+ textInput.value;
+};
+
+
+
